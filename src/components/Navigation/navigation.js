95c07d5c1c2ea81d937ad27b26f60ecd9f6aaca6
justin leggett <justin@codingrookie.com>
2017-07-17 12:06:13 -0500

    navigation working.

--- src/components/Navigation/navigation.js
+++ src/components/Navigation/navigation.js
@@ -1,21 +1,42 @@
 import React, { Component } from "react";
-import "./navigation.css";
-class Navigation extends Component {
+import { Menu } from "antd";
+
+class Navigation extends React.Component {
+  state = {
+    current: "mail"
+  };
+  handleClick = e => {
+    console.log("click ", e);
+    this.setState({
+      current: e.key
+    });
+  };
   render() {
     return (
-      <ul className="nav">
-        <li>
-          <a href="#intro">Intro</a>
-        </li>
-        <li>
-          <a href="#projects">Projects</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
+      <Menu
+        onClick={this.handleClick}
+        selectedKeys={[this.state.current]}
+        mode="horizontal"
+      >
+        <Menu.Item key="alipay2">
+          <a href="#" rel="noopener noreferrer">
+            Intro
+          </a>
+        </Menu.Item>
+        <Menu.Item key="alipay">
+          <a href="#projects" rel="noopener noreferrer">
+            Projects
+          </a>
+        </Menu.Item>
+        <Menu.Item key="alipay1">
+          <a href="#contact" rel="noopener noreferrer">
+            Contact
+          </a>
+        </Menu.Item>
+      </Menu>
     );
   }
 }
 
+// ReactDOM.render(<App />, mountNode);
 export default Navigation;
